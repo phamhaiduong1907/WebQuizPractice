fce701bafde4ecfb73fb4a5fbde9c2020ae10c07
long <long@DESKTOP-AE56H4V>
2022-06-29 22:43:30 +0700

    add model for quiz list & quiz detail

---
+++ src/java/model/Quiz.java
@@ -0,0 +1,114 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author long
+ */
+public class Quiz {
+    private int quizID;
+    private String quizName;
+    private Course course;
+    private Dimension dimension;
+    private QuizLevel level;
+    private int numOfQuestion;
+    private int duration;
+    private float passRate;
+    private String description;
+    private QuizType quizType;
+    private Boolean isTaken;
+
+    public Boolean getIsTaken() {
+        return isTaken;
+    }
+
+    public void setIsTaken(Boolean isTaken) {
+        this.isTaken = isTaken;
+    }
+
+
+    
+    public int getQuizID() {
+        return quizID;
+    }
+
+    public void setQuizID(int quizID) {
+        this.quizID = quizID;
+    }
+
+    public String getQuizName() {
+        return quizName;
+    }
+
+    public void setQuizName(String quizName) {
+        this.quizName = quizName;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public Dimension getDimension() {
+        return dimension;
+    }
+
+    public void setDimension(Dimension dimension) {
+        this.dimension = dimension;
+    }
+
+    public QuizLevel getLevel() {
+        return level;
+    }
+
+    public void setLevel(QuizLevel level) {
+        this.level = level;
+    }
+
+    public int getNumOfQuestion() {
+        return numOfQuestion;
+    }
+
+    public void setNumOfQuestion(int numOfQuestion) {
+        this.numOfQuestion = numOfQuestion;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public float getPassRate() {
+        return passRate;
+    }
+
+    public void setPassRate(float passRate) {
+        this.passRate = passRate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public QuizType getQuizType() {
+        return quizType;
+    }
+
+    public void setQuizType(QuizType quizType) {
+        this.quizType = quizType;
+    }
+    
+}
